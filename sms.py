60fe3938b0f31ffc014234148dab7b6405cb201a
gokulcool5 <60109433+gokulcool5@users.noreply.github.com>
2020-08-10 14:27:37 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
 
 account_sid = 'ACb9d9eccf9c4ec1a1abdcc8d76505bc00'
-auth_token = 'be092a93c18bbdf24c0159791d83285b'
+auth_token = '---------'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
     from_='+18703760938',
     body='this is for fun so no problem',
-    to='+918438888352'
+    to='+91----------'
 )
 
 print(message.sid)
